fbbe7aa646d9462eb89bae7b40efd64dcbdf6dc0
Akashokshelke96 <akashokshelke@gmail.com>
2024-11-23 18:01:24 +0530

    Thread Joining

--- src/Threads/ThreadTesterMain.java
+++ src/Threads/ThreadTesterMain.java
@@ -18,26 +18,41 @@ public class ThreadTesterMain {
 
 
         // Here to learn synchronization we are creating stack and running loops to un-stabilize the stack
-        Stack stack = new Stack(10);
+//        Stack stack = new Stack(10);
+//
+//        new Thread(
+//                () -> {
+//                    int counter = 0 ;
+//                    while(++counter < 10){
+//                        System.out.println("Pushed Element " + stack.push(100));
+//                    }
+//                },"Pusher").start();
+//
+//
+//        new Thread(
+//                ()-> {
+//                    int counter = 0 ;
+//                    while(++counter < 10){
+//                        System.out.println("Popped Element " + stack.pop());
+//                    }
+//                },"Popper").start();
+//
+//        System.out.println(stack.toString());
 
-        new Thread(
+        Thread t4  = new Thread(
                 () -> {
-                    int counter = 0 ;
-                    while(++counter < 10){
-                        System.out.println("Pushed Element " + stack.push(100));
-                    }
-                },"Pusher").start();
+                    System.out.println("Thread is to be joined! " + Thread.currentThread());
+                }
 
+        );
+        t4.start();
 
-        new Thread(
-                ()-> {
-                    int counter = 0 ;
-                    while(++counter < 10){
-                        System.out.println("Popped Element " + stack.pop());
-                    }
-                },"Popper").start();
+        try {
+            t4.join(); // it stops the execution of main thread to run parallely and makes it wait for the t4 internal to complete first.
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-        System.out.println(stack.toString());
         System.out.println("Main Exits!!");
 
     }
